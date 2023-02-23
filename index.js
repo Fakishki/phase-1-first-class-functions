3a825f4c59e4f2a987acc4c95738dc003de53279
Wally Pfingsten <wallypfingsten@gmail.com>
2023-02-22 18:18:58 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,20 @@
+/* function theBaby() {
+    return tue
+} */
+
+function receivesAFunction(dingo) {
+    dingo();
+};
+
+function returnsANamedFunction() {
+    function barbie() {
+        1 + 1;
+    };
+    return barbie;
+};
+
+function returnsAnAnonymousFunction() {
+    return function(){
+        2 + 2
+    };
+};
